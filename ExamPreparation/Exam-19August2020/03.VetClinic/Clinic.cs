4d0ece8175fd3bb009ff51abab682ba350104cec
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-04-07 00:59:53 +0300

    ExamPrep

---
+++ ExamPreparation/Exam-19August2020/03.VetClinic/Clinic.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public void Add(Pet pet)
+        {
+            if (Capacity > this.data.Count)
+            {
+                data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = data.Find(x => x.Name == name);
+
+            if (pet != null)
+            {
+                data.Remove(pet);
+                return true;
+            }
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet surchedPet = data.Find(x => x.Name == name && x.Owner == owner);
+
+            if (data.Contains(surchedPet))
+            {
+                return surchedPet;
+            }
+            return null;
+        }
+
+        public Pet GetOldestPet()
+        {
+            Pet oldestPet = data.OrderByDescending(x => x.Age).First();
+            return oldestPet;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The clinic has the following patients:");
+
+            foreach (var pet in data)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+        public int Capacity { get; set; }
+    }
+}
